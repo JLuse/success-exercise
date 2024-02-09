6a53c2289337b6d0c7f92fca7907882c2af0119d
Jordan <j.luse7@gmail.com>
2024-02-08 16:26:40 -0800

    Updated home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+      <h1>Jordan Luse - Customer Success take home</h1>
     </main>
   );
 }
