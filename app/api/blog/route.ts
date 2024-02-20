34b07679384b006fad9100c72d469d8452966ce8
Jordan Luse <j.luse7@gmail.com>
2024-02-19 17:58:06 -0800

    Another CORs error solutions test

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,16 +1,5 @@
+import { NextResponse } from 'next/server'
 
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<string>) {
-  // Fetch data from the external URL
-  const response = await fetch('https://jluse.github.io/');
-  const data = await response.text();
-
-  // Set the appropriate headers for your response
-  res.setHeader('Content-Type', 'text/html');
-  // Include the Access-Control-Allow-Origin header if you want to allow any origin to access this API route
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  
-  // Send the response back to the client
-  res.status(200).send(data);
+export const GET = async (request: Request) => {
+  return NextResponse.json({ message: 'hello' })
 }
